16e2ed986647dd7b3412bd14e419756447c8cf1d
erennew <erenyeagermikasa84@gmail.com>
2025-04-02 20:22:19 +0530

    config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ TIME = int(os.environ.get("TIME", "900"))
 FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1002650862527"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002331321194"))
 FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001956677010"))
-FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
+FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002508438247"))
 
 # Set the number of workers for the bot
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
