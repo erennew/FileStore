a41ef56a91c1022b178fcc3552c4b16f0262e9de
erennew <erenyeagermikasa84@gmail.com>
2025-04-05 01:31:25 +0530

    start.py

--- plugins/start.py
+++ plugins/start.py
@@ -188,8 +188,7 @@ async def start_command(client: Client, message: Message):
     finally:
         await temp_msg.delete()
 
-    codeflix_msgs = []
-
+        codeflix_msgs = []
         for msg in messages:
             caption = (CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, 
                                              filename=msg.document.file_name) if bool(CUSTOM_CAPTION) and bool(msg.document)
