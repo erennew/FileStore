a850cf98f85d3556e7bf476cede4b9abe9db0609
erennew <erenyeagermikasa84@gmail.com>
2025-04-04 21:37:49 +0530

    config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1047253913"))
 # Port
 PORT = os.environ.get("PORT", "8000")
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://erenyeagermikasa84:<pkbOXb3ulzi9cEFd>@cluster0.ingt8mt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://atlas-sample-dataset-load-67eff8d72541040a56260769:<Elx646yTQK2ubS1l>@cluster0.gmnkeqi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 # Time in seconds for message delete, put 0 to never delete
