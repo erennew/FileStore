cefb1a42485724ff16cddf9a7f29ba7535730346
erennew <erenyeagermikasa84@gmail.com>
2025-04-05 02:09:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -250,31 +250,31 @@ async def start_command(client: Client, message: Message):
                 )
             except Exception as e:
                 print(f"Error updating notification with 'Get File Again' button: {e}")
-    else:
-        reply_markup = InlineKeyboardMarkup(
+             else:
+                reply_markup = InlineKeyboardMarkup(
+                    [
+                            [InlineKeyboardButton("• ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ •", url="https://t.me/CulturedTeluguweeb")],
+        
             [
-                    [InlineKeyboardButton("• ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ •", url="https://t.me/CulturedTeluguweeb")],
-
-    [
-                    InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('ʜᴇʟᴘ •', callback_data = "help")
-
-    ]
+                            InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data = "about"),
+                            InlineKeyboardButton('ʜᴇʟᴘ •', callback_data = "help")
+        
             ]
-        )
-        await message.reply_photo(
-            photo=START_PIC,
-            caption=START_MSG.format(
-                first=message.from_user.first_name,
-                last=message.from_user.last_name,
-                username=None if not message.from_user.username else '@' + message.from_user.username,
-                mention=message.from_user.mention,
-                id=message.from_user.id
-            ),
-            reply_markup=reply_markup#,
-            #message_effect_id=5104841245755180586  # 
-        )
-        return
+                    ]
+                )
+                await message.reply_photo(
+                    photo=START_PIC,
+                    caption=START_MSG.format(
+                        first=message.from_user.first_name,
+                        last=message.from_user.last_name,
+                        username=None if not message.from_user.username else '@' + message.from_user.username,
+                        mention=message.from_user.mention,
+                        id=message.from_user.id
+                    ),
+                    reply_markup=reply_markup#,
+                    #message_effect_id=5104841245755180586  # 
+                )
+                return
 
 
 #=====================================================================================##
